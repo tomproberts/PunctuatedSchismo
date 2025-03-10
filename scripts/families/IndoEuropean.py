50f740a3c75ca8b2d2a285d108428038e384a6e7
Thomas Paul Roberts <tomproberts@outlook.com>
2025-03-10 18:20:45 +0100

    Made slightly better by merging once instead of 1000 times, moved back into PhonologicalDistance.py but imperfect

--- scripts/families/IndoEuropean.py
+++ scripts/families/IndoEuropean.py
@@ -18,8 +18,7 @@ class Italic(LanguageFamily):
         self.language_ids = []
         self.all_forms = None
         self.all_cognates = None
-        self.tmp1 = (None, None)
-        self.tmp2 = (None, None)
+        self.already_merged = False
 
     def load_languages(self):
         all_languages = pd.read_csv(IE_COR_LANGUAGES_CSV)
@@ -28,65 +27,43 @@ class Italic(LanguageFamily):
         self.language_ids = list(italic_languages.ID)
         self.languages = list(italic_languages.Name)
 
+    def get_language_ids(self):
+        if len(self.language_ids) == 0:
+            self.load_languages()
+        return self.language_ids
+
     def get_language_id(self, glottocode):
+        # TODO: replace with getters
         index = super().get_index(glottocode)
         return self.language_ids[index]
 
-    def cache(self, glottocode, forms, first=True):
-        if first:
-            self.tmp1 = (glottocode, forms)
-        else:
-            self.tmp2 = (glottocode, forms)
+    def get_forms(self, glottocode, extended=False):
+        all_forms = self.merge_on_cognate_ids()
+        lang_id = self.get_language_id(glottocode)
+        forms = all_forms[all_forms.Language_ID == lang_id]
+        if extended:
+            return forms
+        return list(forms[IE_COR_FORM_COLUMN])
 
-    def get_from_cache(self, glottocode):
-        g1, f1 = self.tmp1
-        if g1 == glottocode:
-            return f1
-        g2, f2 = self.tmp2
-        if g2 == glottocode:
-            return f2
-        return None
-
-    def get_forms(self, glottocode):
-        filtered = self.get_from_cache(glottocode)
-        if filtered is None:
-            lang_id = self.get_language_id(glottocode)
+    def merge_on_cognate_ids(self):
+        if not self.already_merged:
             all_forms = self.get_ie_cor_forms()
-            filtered = all_forms[all_forms.Language_ID == lang_id][['ID', IE_COR_FORM_COLUMN, 'Parameter_ID']]
-        filtered = filtered.sort_values(by=['Parameter_ID'])
-        return list(filtered[IE_COR_FORM_COLUMN])
-
-    def get_common_forms(self, lang1, lang2):
-        merged1 = self.get_from_cache(lang1)
-        if merged1 is None:
-            lang1_id = self.get_language_id(lang1)
-            all_forms = self.get_ie_cor_forms()
-            all_forms1 = all_forms[all_forms.Language_ID == lang1_id][['ID', IE_COR_FORM_COLUMN, 'Parameter_ID']]
+            all_forms = all_forms[all_forms.Language_ID.isin(self.get_language_ids())][
+                ['ID', 'Language_ID', IE_COR_FORM_COLUMN, 'Parameter_ID']]
             all_cognates = self.get_ie_cor_cognates()
-            merged1 = all_forms1.merge(all_cognates, left_on='ID', right_on='Form_ID', suffixes=('', 'y'))[
-                ['ID', 'Parameter_ID', IE_COR_FORM_COLUMN, 'Cognateset_ID']]
-            self.cache(lang1, merged1, True)
-        merged2 = self.get_from_cache(lang2)
-        if merged2 is None:
-            lang2_id = self.get_language_id(lang2)
-            all_forms = self.get_ie_cor_forms()
-            all_forms2 = all_forms[all_forms.Language_ID == lang2_id][['ID', IE_COR_FORM_COLUMN, 'Parameter_ID']]
-            all_cognates = self.get_ie_cor_cognates()
-            merged2 = all_forms2.merge(all_cognates, left_on='ID', right_on='Form_ID', suffixes=('', 'y'))[
-                ['ID', 'Parameter_ID', IE_COR_FORM_COLUMN, 'Cognateset_ID']]
-            self.cache(lang2, merged2, first=False)
+            merged = all_forms.merge(all_cognates, left_on='ID', right_on='Form_ID', suffixes=('', 'y'))[
+                ['ID', 'Language_ID', 'Parameter_ID', IE_COR_FORM_COLUMN, 'Cognateset_ID']]
+            self.all_forms = merged
+            self.already_merged = True
+        return self.get_ie_cor_forms()
 
-        common_cognates = list(set(merged1.Cognateset_ID) & set(merged2.Cognateset_ID))
-
-        forms1 = list(
-            merged1[merged1.Cognateset_ID.isin(common_cognates)].sort_values(by=['Parameter_ID'])[IE_COR_FORM_COLUMN])
-        forms2 = list(
-            merged2[merged2.Cognateset_ID.isin(common_cognates)].sort_values(by=['Parameter_ID'])[IE_COR_FORM_COLUMN])
-
-        return forms1, forms2
 
     def get_ie_cor_forms(self):
-        return pd.read_csv(IE_COR_FORMS_CSV) if self.all_forms is None else self.all_forms
+        if self.all_forms is None:
+            self.all_forms = pd.read_csv(IE_COR_FORMS_CSV)
+        return self.all_forms
 
     def get_ie_cor_cognates(self):
-        return pd.read_csv(IE_COR_COGNATES_CSV) if self.all_cognates is None else self.all_cognates
+        if self.all_cognates is None:
+            self.all_cognates = pd.read_csv(IE_COR_COGNATES_CSV)
+        return self.all_cognates
--- scripts/predictors/PhonologicalDistance.py
+++ scripts/predictors/PhonologicalDistance.py
@@ -1,14 +1,21 @@
 from statistics import median
-import pandas as pd
-
-from scripts.families.IndoEuropean import Italic
 
 from strsimpy.normalized_levenshtein import NormalizedLevenshtein
 
+from scripts.families.IndoEuropean import Italic, IE_COR_FORM_COLUMN
+
 
 def get_common_forms(family, lang1: str, lang2: str) -> (list[str], list[str]):
-    forms1, forms2 = family.get_common_forms(lang1, lang2)
-    # forms1 = family.get_forms_cognate_with(lang1, lang2)
+    forms1 = family.get_forms(lang1, extended=True)
+    forms2 = family.get_forms(lang2, extended=True)
+
+    common_cognates = list(set(forms1.Cognateset_ID) & set(forms2.Cognateset_ID))
+
+    forms1 = list(
+        forms1[forms1.Cognateset_ID.isin(common_cognates)].sort_values(by=['Parameter_ID'])[IE_COR_FORM_COLUMN])
+    forms2 = list(
+        forms2[forms2.Cognateset_ID.isin(common_cognates)].sort_values(by=['Parameter_ID'])[IE_COR_FORM_COLUMN])
+
     assert len(forms1) == len(forms2)
     return forms1, forms2
 
@@ -30,11 +37,11 @@ def calc_phon_distance(forms1: list[str], forms2: list[str], fold=median) -> flo
 
 if __name__ == '__main__':
     family = Italic()
+    # family.language_ids = [17, 25, 39, 42, 56, 59, 62, 63, 68, 76, 112, 122, 130, 132, 189, 203, 204, 207, 209, 220, 281, 283, 289, 305, 311]
     pairs = [('stan1290', 'fran1269'), ('ital1282', 'neap1235'), ('ital1282', 'mila1243')]  # ('stan1295', 'swis1247')
     for lang1, lang2 in pairs:
         forms1, forms2 = get_common_forms(family, lang1, lang2)
         distance = calc_phon_distance(forms1, forms2)
         print(
             f"Levenshtein Distance between {family.get_language(lang1)} and {family.get_language(lang2)} is {distance}")
-
     print(family.get_forms('stan1290'))
