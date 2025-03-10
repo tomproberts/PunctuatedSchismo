ddd82480c5172bc32e9ae0e3db40057324bc4895
Thomas Paul Roberts <tomproberts@outlook.com>
2025-03-10 19:27:02 +0100

    Refactor

--- scripts/predictors/PhonologicalDistance.py
+++ scripts/predictors/PhonologicalDistance.py
@@ -24,6 +24,7 @@ def get_common_forms(family, lang1: str, lang2: str) -> (list[str], list[str]):
 
 
 def calc_phon_distances(forms1: list[str], forms2: list[str]) -> list[float]:
+    # Calculate normalised levenshtein distances for each pair in aligned lists of cognate forms
     normalized_levenshtein = NormalizedLevenshtein()
     distances = []
     for f1, f2 in zip(forms1, forms2):
@@ -32,20 +33,27 @@ def calc_phon_distances(forms1: list[str], forms2: list[str]) -> list[float]:
 
 
 def calc_phon_distance(forms1: list[str], forms2: list[str], average=median) -> float:
+    # Calculate average normalised levenshtein distance between each pair in aligned lists of cognate forms
     distances = calc_phon_distances(forms1, forms2)
     if len(distances) == 0:
         return 0
     return average(distances)
 
 
-if __name__ == '__main__':
-    family = Italic()
-    pairs = [('stan1290', 'fran1269'), ('ital1282', 'neap1235'), ('ital1282', 'mila1243')]  # ('stan1295', 'swis1247')
+def calculate_distances(family, pairs):
+    # Calculate average phonological distance between pairs of languages (glottocodes)
+    distances = []
     for lang1, lang2 in pairs:
         forms1, forms2 = get_common_forms(family, lang1, lang2)
         distance = calc_phon_distance(forms1, forms2)
-        print(
-            f"Levenshtein Distance between {family.get_language(lang1)} and {family.get_language(lang2)} is {distance}")
+        distances.append(distance)
+    return distances
+
+
+if __name__ == '__main__':
+    family = Italic()
+    pairs = [('stan1290', 'fran1269'), ('ital1282', 'neap1235'), ('ital1282', 'mila1243')]  # ('stan1295', 'swis1247')
+    distances = calculate_distances(family, pairs)
 
     forms1, forms2 = get_common_forms(family, 'stan1290', 'ital1282')
     print(forms1[0:20])
