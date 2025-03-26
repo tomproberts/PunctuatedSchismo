fa53be4fb000a1308896deee19e21b4ad47a5bf8
Thomas Paul Roberts <tomproberts@outlook.com>
2025-03-26 12:01:46 +0100

    Refactored, added summary tree and Italic weighted spikes data

--- scripts/gammaspike/summary_tree.py
+++ scripts/gammaspike/summary_tree.py
@@ -4,35 +4,50 @@ import pandas as pd
 
 from scripts.families.indo_european import Italic
 
-family = Italic()
-tree_nexus_file = NexusReader.from_file('/home/thomas/R/tree-analysis/trees/italic.ccd0')
-translate = tree_nexus_file.trees.translators
-tree2: newick.Node = tree_nexus_file.trees.trees[0].newick_tree
-
-leaves_ascii = []
-leaves_name = []
-leaves_glottocode = []
-leaves_weightedSpikes = []
-leaves_weightedSpikes_median = []
+N_TAXA = 4958
 
 
-def visitor(node: newick.Node) -> None:
-    ascii_name = translate[node.name]
-    glottocode = family.get_glottocode_from_ascii(ascii_name)
-    leaves_ascii.append(ascii_name)
-    leaves_name.append(family.get_language(glottocode))
-    leaves_glottocode.append(glottocode)
-    leaves_weightedSpikes.append(4958 * float(node.properties['weightedSpikes']))
-    leaves_weightedSpikes_median.append(4958 * float(node.properties['weightedSpikes_median']))
+def write_out_data(data, family_name) -> None:
+    pd.DataFrame(data).to_csv(f'data/gammaspike/{family_name}.csv', index=False)
 
 
-tree2.visit(visitor, lambda node: node.is_leaf)
-data = {'label': leaves_ascii,
-        'Name': leaves_name,
-        'Glottocode': leaves_glottocode,
-        'weightedSpikes': leaves_weightedSpikes,
-        'weightedSpikes_median': leaves_weightedSpikes_median}
+def visit_tree(tree_nexus_file) -> dict:
+    translate = tree_nexus_file.trees.translators
+    tree: newick.Node = tree_nexus_file.trees.trees[0].newick_tree
 
-pd.DataFrame(data).to_csv('data/gammaspike/Italic.csv', index=False)
+    leaves_ascii = []
+    leaves_name = []
+    leaves_glottocode = []
+    leaves_weightedSpikes = []
+    leaves_weightedSpikes_median = []
 
-# print(tree2.ascii_art())
+    def visitor(node: newick.Node) -> None:
+        ascii_name = translate[node.name]
+        glottocode = family.get_glottocode_from_ascii(ascii_name)
+        leaves_ascii.append(ascii_name)
+        leaves_name.append(family.get_language(glottocode))
+        leaves_glottocode.append(glottocode)
+
+        leaves_weightedSpikes.append(N_TAXA * float(node.properties['weightedSpikes']))
+        leaves_weightedSpikes_median.append(N_TAXA * float(node.properties['weightedSpikes_median']))
+
+    # visit leaf nodes
+    tree.visit(visitor, lambda node: node.is_leaf)
+
+    return {'label': leaves_ascii,
+            'Name': leaves_name,
+            'Glottocode': leaves_glottocode,
+            'weightedSpikes': leaves_weightedSpikes,
+            'weightedSpikes_median': leaves_weightedSpikes_median}
+
+
+def get_summary_tree_nexus(family_name) -> NexusReader:
+    return NexusReader.from_file(f'data/summarytree/{family_name}.nex')
+
+
+if __name__ == '__main__':
+    family = Italic()
+    tree_nexus_file = get_summary_tree_nexus(family.name)
+    data = visit_tree(tree_nexus_file)
+
+    write_out_data(data, family.name)
