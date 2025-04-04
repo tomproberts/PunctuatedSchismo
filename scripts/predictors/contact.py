475b80ab50c66b99c3e134b3f4e0eb672c71ba00
Thomas Paul Roberts <tomproberts@outlook.com>
2025-04-04 16:04:16 +0200

    Calculate shortest intersecting lines from points in one polygon to the edge of the other

--- scripts/predictors/contact.py
+++ scripts/predictors/contact.py
@@ -1,13 +1,24 @@
 import geopandas
 import pandas as pd
-import shapely.plotting
 from matplotlib import pyplot as plt
+from shapely.geometry.linestring import LineString
 from shapely.geometry.point import Point
+from shapely.ops import nearest_points
 
 from scripts.families.indo_european import Italic
 from scripts.glottolog.trees import glottolog_cherries
 from scripts.predictors.polygons.glottography import Glottography
 from scripts.predictors.polygons.glottography_config import get_config
+
+
+def find_closest_point(start, polygon):
+    polygon = polygon['geometry']
+    if polygon.contains(start).iloc[0]:
+        return start
+    p1, _ = nearest_points(polygon, start)
+    return p1.geometry.iloc[0]
+    # return Point(1575000, 5172177)
+
 
 if __name__ == '__main__':
     family = Italic()
@@ -21,12 +32,18 @@ if __name__ == '__main__':
     combined = pd.concat([oscan, umbrian])
     base = combined.plot(color='white', edgecolor='black')
     combined.apply(lambda x: base.annotate(text=x['name'], xy=x.geometry.centroid.coords[0], ha='center'), axis=1)
-    umbrian_points = umbrian.sample_points(100, rng=10)
+    umbrian_points = umbrian.sample_points(10, rng=10).explode(index_parts=True)
     # oscan_points = oscan.sample_points(100)
     umbrian_points.plot(ax=base, markersize=1)
 
-    point = Point(1500000, 5252177)
-    geo_point = geopandas.GeoDataFrame(index=[0], geometry=[point])
-    geo_point.plot(ax=base, color='red')
+    # point = Point(1600000, 5152177)
+    distances = []
+    for i in range(len(umbrian_points)):
+        point = umbrian_points.geometry.iloc[i]
+        end = find_closest_point(point, oscan)
+        distance = LineString([point, end])
+        distances.append(distance)
+    lines = geopandas.GeoDataFrame(index=list(range(len(distances))), geometry=distances)
+    lines.plot(ax=base, color='red')
 
     plt.show()
