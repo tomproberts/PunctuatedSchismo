216fa48533da45a365d613b8a68b6509e3739bd7
Thomas Paul Roberts <tomproberts@outlook.com>
2025-04-04 18:12:44 +0200

    Write out contact distances for Italic

--- scripts/predictors/contact.py
+++ scripts/predictors/contact.py
@@ -9,14 +9,14 @@ from shapely.ops import nearest_points
 
 from scripts.families.indo_european import Italic
 from scripts.glottolog.trees import glottolog_cherries
-from scripts.predictors.polygons.glottography import Glottography
+from scripts.predictors.polygons.glottography import Glottography, LiterallyNoPolygonException
 from scripts.predictors.polygons.glottography_config import get_config
 from scripts.predictors.utils import write_out_df
 
 CONTACT_DIR = 'contact'
 
 SEED = None
-N_POINTS = 50
+N_POINTS = 500
 GRAPH = False
 
 
@@ -60,7 +60,7 @@ def sample_points(polygon):
 
 
 def write_out_contact(family_name, cherries, mean_distances, median_distances):
-    file_name = f'{family_name}.contact'
+    file_name = f'{family_name}.contact.euclidean'
     assert len(cherries) == len(median_distances) == len(median_distances)
     dataframe = pd.DataFrame(data={
         'language_1': [ls[0] for ls in cherries],
@@ -71,23 +71,24 @@ def write_out_contact(family_name, cherries, mean_distances, median_distances):
     write_out_df(CONTACT_DIR, file_name, dataframe)
 
 
-if __name__ == '__main__':
-    family = Italic()
-    cherries = glottolog_cherries(family)
-    cherries = [cherries[-1], cherries[2], cherries[-2]]
-
-    glottography = Glottography(get_config(family.name))
+def calculate_euclidean_distances(cherries, glottography):
     mean_distances = []
     median_distances = []
+    new_cherries = []
     for (language_1, language_2) in cherries:
-        polygon_1 = glottography.get_polygon(language_1)
-        polygon_2 = glottography.get_polygon(language_2)
+        try:
+            polygon_1 = glottography.get_polygon(language_1)
+            polygon_2 = glottography.get_polygon(language_2)
+        except LiterallyNoPolygonException as e:
+            print(f'Warning: {e} (skipping)')
+            continue
 
         umbrian_points = sample_points(polygon_2)
 
         paths = calculate_lines(umbrian_points, polygon_1)
         distances = [p.length for p in paths]
 
+        new_cherries.append((language_1, language_2))
         median_distances.append(median(distances))
         mean_distances.append(mean(distances))
 
@@ -97,4 +98,21 @@ if __name__ == '__main__':
             draw_lines(paths, base)
             plt.show()
 
+    return new_cherries, mean_distances, median_distances
+
+
+def double_reverse(cherries):
+    reversed_cherries = [c[::-1] for c in cherries]
+    return cherries + reversed_cherries
+
+
+if __name__ == '__main__':
+    family = Italic()
+    cherries = glottolog_cherries(family)
+    # cherries = [cherries[-1], cherries[2], cherries[-2]]
+    cherries = double_reverse(cherries)
+
+    glottography = Glottography(get_config(family.name))
+    cherries, mean_distances, median_distances = calculate_euclidean_distances(cherries, glottography)
     write_out_contact(family.name, cherries, mean_distances, median_distances)
+    print(f'Wrote out contact distances for {family.name}')
