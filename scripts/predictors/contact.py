d813f088c8b61c4cdd03b61f523a60e0520faec0
Thomas Paul Roberts <tomproberts@outlook.com>
2025-04-04 17:50:24 +0200

    Write out contact distances

--- scripts/predictors/contact.py
+++ scripts/predictors/contact.py
@@ -1,3 +1,5 @@
+from statistics import median, mean
+
 import geopandas
 import pandas as pd
 from matplotlib import pyplot as plt
@@ -9,9 +11,13 @@ from scripts.families.indo_european import Italic
 from scripts.glottolog.trees import glottolog_cherries
 from scripts.predictors.polygons.glottography import Glottography
 from scripts.predictors.polygons.glottography_config import get_config
+from scripts.predictors.utils import write_out_df
+
+CONTACT_DIR = 'contact'
 
 SEED = None
 N_POINTS = 50
+GRAPH = False
 
 
 def find_closest_point(start, polygon) -> Point:
@@ -53,20 +59,42 @@ def sample_points(polygon):
     return polygon.sample_points(N_POINTS, rng=SEED).explode(index_parts=True)
 
 
+def write_out_contact(family_name, cherries, mean_distances, median_distances):
+    file_name = f'{family_name}.contact'
+    assert len(cherries) == len(median_distances) == len(median_distances)
+    dataframe = pd.DataFrame(data={
+        'language_1': [ls[0] for ls in cherries],
+        'language_2': [ls[1] for ls in cherries],
+        'median_contact': median_distances,
+        'mean_contact': mean_distances
+    })
+    write_out_df(CONTACT_DIR, file_name, dataframe)
+
+
 if __name__ == '__main__':
     family = Italic()
     cherries = glottolog_cherries(family)
-    (oscan_glottocode, umbrian_glottocode) = cherries[-1]
+    cherries = [cherries[-1], cherries[2], cherries[-2]]
 
     glottography = Glottography(get_config(family.name))
-    oscan = glottography.get_polygon(oscan_glottocode)
-    umbrian = glottography.get_polygon(umbrian_glottocode)
+    mean_distances = []
+    median_distances = []
+    for (language_1, language_2) in cherries:
+        polygon_1 = glottography.get_polygon(language_1)
+        polygon_2 = glottography.get_polygon(language_2)
 
-    base = graph_base_polygons(oscan, umbrian)
-    umbrian_points = sample_points(umbrian)
+        umbrian_points = sample_points(polygon_2)
 
-    # plot_points(umbrian_points, base)
-    distances = calculate_lines(umbrian_points, oscan)
+        paths = calculate_lines(umbrian_points, polygon_1)
+        distances = [p.length for p in paths]
 
-    draw_lines(distances, base)
-    plt.show()
+        median_distances.append(median(distances))
+        mean_distances.append(mean(distances))
+
+        if GRAPH:
+            base = graph_base_polygons(polygon_1, polygon_2)
+            # plot_points(umbrian_points, base)
+            draw_lines(paths, base)
+            plt.show()
+
+    write_out_contact(family.name, cherries, mean_distances, median_distances)
