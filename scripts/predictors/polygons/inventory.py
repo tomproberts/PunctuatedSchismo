93c59a5acb69403728348a5aff220263f1db89ac
Thomas Paul Roberts <tomproberts@outlook.com>
2025-05-06 14:28:44 +0200

    Provide summary in polygon inventory script

--- scripts/predictors/polygons/inventory.py
+++ scripts/predictors/polygons/inventory.py
@@ -1,10 +1,11 @@
+from scripts.families.dravidian import Dravidian
 from scripts.families.indo_european import IndoEuropean
 from scripts.gammaspike.summary_tree import summary_tree_cherries
 from scripts.predictors.polygons.glottography import Glottography, LiterallyNoPolygonException
 from scripts.predictors.polygons.glottography_config import get_config
 
 if __name__ == '__main__':
-    family = IndoEuropean()
+    family = Dravidian()
     glottography = Glottography(get_config(family.name))
 
     # Find missing
@@ -18,7 +19,8 @@ if __name__ == '__main__':
     # Find consequences
     half_cherries = []
     both_missing = []
-    for (l1, l2) in summary_tree_cherries(family):
+    summary_cherries = summary_tree_cherries(family)
+    for (l1, l2) in summary_cherries:
         if l1 in not_present:
             if l2 not in not_present:
                 half_cherries.append(l1)
@@ -28,13 +30,22 @@ if __name__ == '__main__':
             half_cherries.append(l2)
 
     # Output consequences
+    n_langs = len(family.languages)
+    print(f'### *{family.name}* ({n_langs} taxa)')
+
+    print('\nSummary:')
+    n_complete_cherries = len(summary_cherries) - len(half_cherries) - len(both_missing)
+    print(f'- {n_complete_cherries} out of {len(summary_cherries)} cherries present')
+    print(f'- {n_langs - len(not_present)} out of {n_langs} polygons present')
+
     if len(half_cherries) > 0:
-        print(f'{len(half_cherries)} broken cherries (only one polygon out of two):')
+        print(f'\n{len(half_cherries)} broken cherries (only one polygon out of two):')
         half_cherries = set(half_cherries)
         for l in half_cherries:
             print(f'- †cherry, because {family.get_language(l)} ({l}) missing')
 
     if len(both_missing) > 0:
-        print(f'{len(both_missing)} dead cherries (neither polygon present):')
+        print(f'\n{len(both_missing)} dead cherries (neither polygon present):')
         for (l1, l2) in both_missing:
-            print(f'- †cherry, because both {family.get_language(l1)} ({l1}) and {family.get_language(l2)} ({l2}) missing')
+            print(
+                f'- †cherry, because both {family.get_language(l1)} ({l1}) and {family.get_language(l2)} ({l2}) missing')
