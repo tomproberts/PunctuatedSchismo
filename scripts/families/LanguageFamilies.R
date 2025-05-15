e38658be16341120d2a8a48c2d50a760d5998b67
Thomas Paul Roberts <tomproberts@outlook.com>
2025-05-15 13:38:04 +0200

    Added preliminary uralic phylogeny

--- scripts/families/LanguageFamilies.R
+++ scripts/families/LanguageFamilies.R
@@ -4,7 +4,7 @@ DRAVIDIAN <- "Dravidian"
 URALIC <- "Uralic"
 
 get_n_sites <- function(family) {
-  if (family == ITALIC || family == INDO.EUROPEAN) 4958
-  if (family == DRAVIDIAN) 877
-  if (family == URALIC) 3655
+  if (family == ITALIC || family == INDO.EUROPEAN) return (4958)
+  if (family == DRAVIDIAN) return (877)
+  if (family == URALIC) return (3655)
 }
