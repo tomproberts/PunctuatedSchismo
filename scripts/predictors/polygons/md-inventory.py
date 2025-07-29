04d01321608659721de1b35534fb8ddadeed26d7
Thomas Paul Roberts <tomproberts@outlook.com>
2025-07-29 15:37:21 +0200

    Factored out glottography dataset names to constants, found some more polygons for Uto-Aztecan

--- scripts/predictors/polygons/md-inventory.py
+++ scripts/predictors/polygons/md-inventory.py
@@ -1,4 +1,5 @@
-from scripts.families.bantu import Bantu
+from scripts.families.sino_tibetan import SinoTibetan
+from scripts.families.uto_aztecan import UtoAztecan
 from scripts.gammaspike.summary_tree import get_sorted_summary_tree_cherries_ascii
 from scripts.predictors.polygons.glottography import Glottography
 from scripts.predictors.polygons.glottography_config import get_config
@@ -65,7 +66,7 @@ def polygon_inventory_markdown(family, glottography):
 
 if __name__ == '__main__':
     # Load family
-    family = Bantu()
+    family = SinoTibetan()
     glottography = Glottography(get_config(family.name))
 
     # Generate markdown report
