b988c8b2d2d429acc8c0e136a4821d95fa72e1bd
Thomas Paul Roberts <tomproberts@outlook.com>
2025-08-07 16:04:53 +0200

    Added first Uto-Aztecan burst phylogeny, graph bursts etc.

--- scripts/plots/trees/PlotSummaryTree.R
+++ scripts/plots/trees/PlotSummaryTree.R
@@ -3,14 +3,12 @@ library(ggtree)
 library(treeio)
 source("scripts/families/LanguageFamilies.R")
 
-# Think about how number of taxa affects gammaShape etc.
-FAMILY <- SINO.TIBETAN
+FAMILY <- UTO.AZTECAN
 SCALESTUBS <- TRUE
 
 summary.tree <- paste0("data/gammaspike/summarytree/", FAMILY, ".nex")
 tree <- read.beast(summary.tree)
 df <- as_tibble(tree)
-df$scaled_spikes <- df$weightedSpikes_median * get_n_sites(FAMILY) / 2
 
 aesthetics <- if (SCALESTUBS)
   aes(color = weightedSpikes_median / (nstubs_median + 1)) else
