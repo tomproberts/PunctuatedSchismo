7296989f8d84fed9e131ee62b65035c01104cbaa
Thomas Paul Roberts <tomproberts@outlook.com>
2025-09-04 11:36:03 +0200

    Refactored contact.py script to use ascii names glottolog tree instead of glottocodes, since ascii are unique

--- scripts/predictors/contact.py
+++ scripts/predictors/contact.py
@@ -6,11 +6,12 @@ from matplotlib import pyplot as plt
 from shapely.geometry.linestring import LineString
 from shapely.geometry.point import Point
 from shapely.ops import nearest_points
+from tqdm import tqdm
 
 from scripts.families.indo_european import IndoEuropean
 from scripts.families.utils import LanguageFamily
-from scripts.glottolog.trees import glottolog_cherries
-from scripts.predictors.polygons.glottography import Glottography, LiterallyNoPolygonException
+from scripts.glottolog.trees import glottolog_cherries, GlottologTreeType
+from scripts.predictors.polygons.glottography import Glottography
 from scripts.predictors.polygons.glottography_config import get_config
 from scripts.predictors.utils import write_out_df
 
@@ -28,7 +29,7 @@ def find_closest_point(start, polygon) -> Point:
     return p1
 
 
-def calculate_lines(points, other_polygon) -> [LineString]:
+def calculate_lines(points, other_polygon) -> list[LineString]:
     distances = []
     geometry = points.geometry
     for i in range(len(points)):
@@ -47,26 +48,29 @@ def write_out_contact(family, cherries, mean_distances, median_distances, type):
     file_name = f'{family.name}.contact.{type}'
     assert len(cherries) == len(median_distances) == len(median_distances)
     dataframe = pd.DataFrame(data={
-        'language_1': [family.get_language_ascii_from_glottocode(ls[0]) for ls in cherries],
-        'language_2': [family.get_language_ascii_from_glottocode(ls[1]) for ls in cherries],
-        'Glottocode_1': [ls[0] for ls in cherries],
-        'Glottocode_2': [ls[1] for ls in cherries],
+        'language_1': [ls[0] for ls in cherries],
+        'language_2': [ls[1] for ls in cherries],
+        'Glottocode_1': [family.get_glottocode_from_ascii(ls[0]) for ls in cherries],
+        'Glottocode_2': [family.get_glottocode_from_ascii(ls[1]) for ls in cherries],
         'median_distance': median_distances,
         'mean_distance': mean_distances
     })
     write_out_df(CONTACT_DIR, file_name, dataframe)
 
 
-def calculate_euclidean_distances(cherries, glottography):
+def calculate_euclidean_distances(family, cherries, glottography):
     mean_distances = []
     median_distances = []
     new_cherries = []
-    for (language_1, language_2) in cherries:
+    errors = []
+    for (language_1, language_2) in tqdm(cherries):
         try:
-            polygon_1 = glottography.get_polygon(language_1)
-            polygon_2 = glottography.get_polygon(language_2)
-        except LiterallyNoPolygonException as e:
-            print(f'Warning: {e} (skipping)')
+            glottocode_1 = family.get_glottocode_from_ascii(language_1)
+            glottocode_2 = family.get_glottocode_from_ascii(language_2)
+            polygon_1 = glottography.get_polygon(glottocode_1)
+            polygon_2 = glottography.get_polygon(glottocode_2)
+        except Exception as e:
+            errors.append(e)
             continue
 
         sampled_points = sample_points(polygon_1)
@@ -77,6 +81,10 @@ def calculate_euclidean_distances(cherries, glottography):
         new_cherries.append((language_1, language_2))
         median_distances.append(median(distances))
         mean_distances.append(mean(distances))
+
+    # show skipped languages
+    for e in errors:
+        print(f'Warning: {e} (ignored)')
 
     return new_cherries, mean_distances, median_distances
 
@@ -108,9 +116,10 @@ def plot_contact(language_1, language_2, glottography, n_points):
 
 
 def calculate_output_contact(family: LanguageFamily, glottography):
-    cherries = glottolog_cherries(family)
+    # Cherries should be ascii names since they are unique
+    cherries = glottolog_cherries(family, type=GlottologTreeType.ASCII)
     cherries = double_reverse(cherries)
-    cherries, mean_distances, median_distances = calculate_euclidean_distances(cherries, glottography)
+    cherries, mean_distances, median_distances = calculate_euclidean_distances(family, cherries, glottography)
     # TODO: include language IDs
     write_out_contact(family, cherries, mean_distances, median_distances, type="geodesic")
     print(f'Wrote out contact distances for {family.name}')
