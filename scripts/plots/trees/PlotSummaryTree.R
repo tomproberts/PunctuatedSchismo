5208f8e1a335c60cc32302c81809fa13a4b2b091
Thomas Paul Roberts <tomproberts@outlook.com>
2025-10-22 14:39:04 +0200

    Fixed tree plotting to work for NAs present in Pama-Nyungan summary tree

--- scripts/plots/trees/PlotSummaryTree.R
+++ scripts/plots/trees/PlotSummaryTree.R
@@ -3,8 +3,8 @@ library(ggtree)
 library(treeio)
 source("scripts/families/LanguageFamilies.R")
 
-FAMILY <- UTO.AZTECAN
-GAMMASPIKE <- TRUE
+FAMILY <- PAMA.NYUNGAN
+GAMMASPIKE <- FALSE
 SCALESTUBS <- TRUE
 
 summary.tree <- paste0("data/", if (GAMMASPIKE) "gammaspike/summarytree/" else "relaxed/", FAMILY, ".nex")
@@ -14,12 +14,14 @@ df <- as_tibble(tree)
 aesthetics <- if (GAMMASPIKE) (if (SCALESTUBS)
   aes(color = weightedSpikes_median / (nstubs_median + 1)) else
   aes(color = weightedSpikes_median)) else
-  aes(color = log(rate_median))
+  aes(color = log(as.numeric(rate_median)))
 
 ggtree(tree, aesthetics, size = 2,) +
-  # theme_tree2() +  # enable axis
+  theme_tree2() +  # enable axis
   scale_color_continuous(low = "black", high = if (GAMMASPIKE) "#00bfc4" else "#8494ff") +
   geom_tiplab(color = "black", align = TRUE, as_ylab = TRUE, size = 16) +
-  (if (GAMMASPIKE) geom_label2(aes(subset = nstubs_median > 0, x = branch, label = nstubs_median), fill = "black", color = "white", alpha = .5)) +
-  geom_label2(aes(subset = posterior < 1, label = round(posterior, 2)), fill = "white", color = "black", alpha = .8) +
+  (if (GAMMASPIKE) geom_label2(aes(subset = nstubs_median > 0, x = branch, label = nstubs_median),
+                               fill = "black", color = "white", alpha = .5)) +
+  geom_label2(aes(subset = posterior < 1, label = round(as.numeric(posterior), 2)),
+              fill = "white", color = "black", alpha = .8) +
   theme(legend.position = "none")
