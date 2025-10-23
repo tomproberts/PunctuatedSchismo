0a7db9c68b16c47a123408d774f8fdaac12437d3
Thomas Paul Roberts <tomproberts@outlook.com>
2025-10-23 15:00:10 +0200

    Ran relaxed glm for Pama-Nyungan, seems to show opposite effect to expected

---
+++ scripts/glm/RelaxedGLM.R
@@ -0,0 +1,75 @@
+library(brms)
+library(marginaleffects)
+library(ggplot2)
+library(bayesplot)
+library(collapse)
+source("scripts/gammaspike/SummaryTree.R")
+
+rates <- read.csv("data/relaxed/PamaNyungan.csv")
+areas <- read.csv("data/predictors/area/PamaNyungan.geodesic.csv")
+distances <- read.csv("data/predictors/contact/PamaNyungan.contact.500.csv")
+
+cherries <- get_summary_cherries(PAMA.NYUNGAN)
+lang1 <- sapply(cherries, function(e) return(e[1]))
+lang2 <- sapply(cherries, function(e) return(e[2]))
+
+# Init df
+df <- data.frame(matrix(ncol = 0, nrow = (2 * length(cherries))))
+df$lang <- append(lang1, lang2)
+df$lang_sister <- append(lang2, lang1)
+df$cherry <- append(seq_along(lang1), seq_along(lang2))
+
+# Rates
+df <- merge(df, rates, by.x = "lang", by.y = "label")
+
+# Areas
+df <- merge(df, areas, by.x = "lang", by.y = "lang")
+
+# Contact
+df <- merge(df, distances, by.x = c("lang", "lang_sister"), by.y = c("language_1", "language_2"))
+
+# Sisters
+df <- merge(df, df[, c("lang", "rate_median", "area", "median_distance")],
+            by.x = "lang_sister", by.y = "lang", suffixes = c("", "_sister"))
+
+EPSILON <- 1
+df$area_ratio <- log(df$area / df$area_sister)
+df <- df[df$median_distance != 0,]
+df$median_distance[df$median_distance < EPSILON] <- EPSILON
+df$log_median_distance <- log(df$median_distance)
+df$normalised_rate <- df$rate / mean(df$rate)
+
+if (FALSE) {
+  ggplot(df, aes(x = log(area), y = log(area_sister))) +
+    geom_text(data = df, aes(label = name), size = 4) +
+    geom_smooth(method = "lm", formula = y ~ x) +
+    # ylim(1, 200) +
+    theme_classic()
+}
+
+# Fit the model
+if (TRUE) {
+  fit <- brm(formula = normalised_rate ~
+    log(area_sister) +
+      log(median_distance),
+      # log(area),
+             family = gaussian(link = "log"),
+             data = df,
+             iter = 4000)
+  save(fit, file = "data/glm/PamaNyungan2.RData")
+  # Graph brms
+  print(summary(fit))
+}
+
+if (TRUE) {
+  plot_predictions(fit, condition = "area_sister", allow_new_levels = TRUE) +
+    # geom_text(aes(x = median_distance_km, y = response, label = Name), position = position_nudge(y = -0.08), data = df, size = 4) +
+    # geom_point(aes(x = median_distance_km, y = response), data = df, size = 1) +
+    xlab("Area of sibling [~km^2]") +
+    ylab("Normalised branch rate") +
+    ggtitle("normalised_rate ~ area_sister") +
+    # xlim(0, 300000) +
+    theme_classic() +
+    theme(axis.title = element_text(size = 14))
+}
+
--- scripts/plots/glm/Coefficients.R
+++ scripts/plots/glm/Coefficients.R
@@ -3,7 +3,7 @@ library(ggdist)
 library(tidyr)
 library(dplyr)
 
-load("data/glm/Douglas.RData")
+load("data/glm/PamaNyungan.RData")
 
 par.names <- c(
   "log(median distance)" = "b_logmedian_distance",
