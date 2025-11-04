9444056ce95f1c445c44d650b53e0b228977150c
Thomas Paul Roberts <tomproberts@outlook.com>
2025-11-04 15:05:41 +0100

    Added basic naïve script to calculate average distance from water speakers in a language polygon are away from, save as csv in predictors

--- scripts/predictors/water.py
+++ scripts/predictors/water.py
@@ -1,4 +1,5 @@
 import time
+from statistics import median
 
 import geopandas
 import pandas as pd
@@ -6,10 +7,15 @@ from pyogrio.errors import DataSourceError
 from tqdm import tqdm
 
 from scripts.families.pama_nyungan import PamaNyungan
+from scripts.predictors.contact import sample_points
 from scripts.predictors.polygons.australia.pama_nyungan_polygons import PamaNyunganPolygons
+from scripts.predictors.polygons.glottography import LiterallyNoPolygonException
+
+N_POINTS = 50
 
 
 def read_or_create_relevant_water(family, glottography):
+    print('Loading water features in language areas...')
     try:
         start = time.time()
         relevant = geopandas.read_file('data/water/PamaNyunganWaterIntersect.gpkg')
@@ -25,7 +31,7 @@ def read_or_create_relevant_water(family, glottography):
             p = glottography.get_polygon_from_ascii(family, ascii)
             dataframesList.append(p)
         except Exception as e:
-            pass
+            print(e)
 
     crs = dataframesList[0].crs
     polygons = geopandas.GeoDataFrame(pd.concat(dataframesList, ignore_index=True), crs=crs)
@@ -33,12 +39,12 @@ def read_or_create_relevant_water(family, glottography):
     polygons = geopandas.GeoDataFrame(geometry=polygons, crs=crs)
     super_polygon = polygons.dissolve(as_index=False).to_crs('EPSG:4283')
     # super_polygon.to_file('data/water/PamaNyunganSuper.geojson', driver='GeoJSON')
-    print('Constructed super polygon!')
+    print('Constructed super polygon! Loading water features...')
 
     water = geopandas.read_file('data/water/SurfaceHydrologyPolygonsNational.gdb')
-    print('Loaded water')
-    relevant = water.overlay(super_polygon, how='intersection')
-    print('Calculated intersection')
+    print('Loaded water. Calculating intersection...')
+    relevant = water.overlay(super_polygon, how='intersection').to_crs('EPSG:32633')
+    print('Calculated intersection. Writing to file...')
     relevant.to_file('data/water/PamaNyunganWaterIntersect.gpkg', driver='GPKG')
     print('Wrote out intersection')
     print(f'Filtered water in language areas in {time.time() - start} seconds')
@@ -53,14 +59,32 @@ if __name__ == '__main__':
     relevant = read_or_create_relevant_water(family, glottography)
 
     dataframesList = []
+    df_dict = []
     for ascii in tqdm(family.languages_ascii):
         try:
-            p = glottography.get_polygon_from_ascii(family, ascii).to_crs('EPSG:4283')
-            intersecting = p.overlay(relevant, how='intersection')
-            merged = intersecting.dissolve(as_index=False).to_crs('EPSG:4283')
-            dataframesList.append(merged.filter(['Name', 'geometry']))
-        except Exception as e:
+            p = glottography.get_polygon_from_ascii(family, ascii).to_crs('EPSG:32633')
+            intersecting = p.overlay(relevant, how='intersection', keep_geom_type=False)
+            merged = intersecting.dissolve(as_index=False).to_crs('EPSG:32633')
+            merged = merged.filter(['Name', 'geometry'])
+
+            points = sample_points(p, N_POINTS)
+
+            d = points.apply(lambda point: merged.disance(point))[0]
+            median_dist = median(d)
+            df_dict.append({
+                'ascii': ascii,
+                'distance': median_dist
+            })
+            merged['waterdistance'] = median_dist
+            dataframesList.append(merged)
+        except LiterallyNoPolygonException:
             pass
+        except Exception as e:
+            print(e)
 
     rdf = geopandas.GeoDataFrame(pd.concat(dataframesList, ignore_index=True), crs=dataframesList[0].crs)
     rdf.to_file('data/water/LanguageMapRiver.gpkg', driver='GPKG')
+
+    df = pd.DataFrame.from_records(df_dict)
+    file_name = f'{family.name}.water.all'
+    df.to_csv(f'data/water/{file_name}.csv', index=False, header=True)
