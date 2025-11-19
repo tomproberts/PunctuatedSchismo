6962aa79f95e2eacdca493bed9344903ef50ba4a
Thomas Paul Roberts <tomproberts@outlook.com>
2025-11-19 15:45:38 +0100

    Reran GLM for Indo-European and Pama-Nyungan, fix logic in coefficients script to graph coefficients in order

--- scripts/plots/glm/Coefficients.R
+++ scripts/plots/glm/Coefficients.R
@@ -3,18 +3,18 @@ library(ggdist)
 library(tidyr)
 library(dplyr)
 
-FIT <- "data/glm/PamaNyunganRelaxed.RData"
+FIT <- "data/glm/IndoEuropean.RData"
 
 par.names <- c(
-  "log(median distance from sister)" = "b_logmedian_distance",
-  "log(area)" = "b_logarea",
-  "log(area of sister)" = "b_logarea_sister",
-  "log(area/area of sister)" = "b_area_ratio",
   "log(total pages)" = "b_log_total_pages",
+  "log(area)" = "b_logarea",
+  "log(sister's area)" = "b_logarea_sister",
+  "log(area/area of sister)" = "b_area_ratio",
   "log(total pages of sister)" = "b_log_total_page_sister",
   "number of loans" = "b_n_loans",
   "log(distance to water)" = "b_logmedian_distance_water",
-  "log(sister's distance to water)" = "b_logmedian_distance_water_sister"
+  "log(sister's distance to water)" = "b_logmedian_distance_water_sister",
+  "log(distance from sister)" = "b_logmedian_distance"
 )
 
 exclude <- c(
@@ -32,20 +32,18 @@ if (length(pars) == 1) {
 } else {
   draws.df <- pivot_longer(rename(draws, any_of(par.names)), cols = everything(), names_to = "coefficient", values_to = "value")
 }
+draws.df$coefficient <- factor(draws.df$coefficient, levels = names(par.names))
 
 ggplot(draws.df, aes(x = value, y = coefficient)) +
   theme_light() +
   theme(legend.position = "none",
-        axis.text = element_text(size = 10),
+        axis.text = element_text(size = 16),
         axis.title = element_text(size = 12)) +
   stat_interval(aes(interval_alpha = after_stat(level)),
                 .width = c(0.5, 0.89, 1),
                 interval_colour = "#00bfc4", linewidth = 6) +
   geom_vline(xintercept = 0, linetype = "dashed", color = "darkgrey") +
+  xlim(-0.25, 0.25) +
   ggtitle(FIT) +
-  xlab("strength of predictor") +
+  xlab("effect on punctuated change") +
   ylab("")
-
-# mcmc_areas(fit, prob = 0.89, pars = pars)
-
-# pp_check(fit, type = "dens_overlay", ndraws = 100)
