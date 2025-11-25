5a3f335b8b978ce63af21bef4865a556351b0700
Thomas Paul Roberts <tomproberts@outlook.com>
2025-11-25 16:00:31 +0100

    Messily adjust script to run on OSM data for Indo-European, takes way too long to actually run though

--- scripts/predictors/water.py
+++ scripts/predictors/water.py
@@ -6,14 +6,15 @@ import pandas as pd
 from pyogrio.errors import DataSourceError
 from tqdm import tqdm
 
-from scripts.families.pama_nyungan import PamaNyungan
+from scripts.families.indo_european import IndoEuropean
 from scripts.predictors.contact import sample_points
-from scripts.predictors.polygons.australia.pama_nyungan_polygons import PamaNyunganPolygons
-from scripts.predictors.polygons.glottography import LiterallyNoPolygonException
+from scripts.predictors.polygons.glottography import LiterallyNoPolygonException, Glottography
+from scripts.predictors.polygons.glottography_config import get_config
 
 N_POINTS = 50
 
 GEODESIC = 'EPSG:32633'
+LONGLAT = 'EPSG:4087'
 filter_type = 'perennial'
 
 def read_or_create_relevant_water(family, glottography, water_file):
@@ -27,23 +28,10 @@ def read_or_create_relevant_water(family, glottography, water_file):
     except DataSourceError as _:
         print('No cached file found for water filtered to language areas')
 
+    super_polygon = get_super_polygon(family, glottography)
+    print('Loading water features...')
+
     start = time.time()
-    dataframesList = []
-    for ascii in family.languages_ascii:
-        try:
-            p = glottography.get_polygon_from_ascii(family, ascii)
-            dataframesList.append(p)
-        except Exception as e:
-            print(e)
-
-    crs = dataframesList[0].crs
-    polygons = geopandas.GeoDataFrame(pd.concat(dataframesList, ignore_index=True), crs=crs)
-    polygons = polygons.make_valid()
-    polygons = geopandas.GeoDataFrame(geometry=polygons, crs=crs)
-    super_polygon = polygons.dissolve(as_index=False).to_crs('EPSG:4283')
-    # super_polygon.to_file('data/water/PamaNyunganSuper.geojson', driver='GeoJSON')
-    print('Constructed super polygon! Loading water features...')
-
     water = geopandas.read_file(f'data/water/{water_file}')
     if filter_type == 'perennial':
         # filter for perennial
@@ -60,26 +48,76 @@ def read_or_create_relevant_water(family, glottography, water_file):
     return relevant
 
 
+def get_super_polygon(family, glottography, save_super=False):
+    start = time.time()
+    dataframesList = []
+    for ascii in family.languages_ascii:
+        try:
+            p = glottography.get_polygon_from_ascii(family, ascii)
+            p.loc[p.index[0], 'geometry'] = p.make_valid(method="structure").iloc[0]
+            dataframesList.append(p)
+        except Exception as e:
+            print(e)
+
+    concatted = pd.concat(dataframesList, ignore_index=True)
+    polygons = geopandas.GeoDataFrame(concatted, crs=dataframesList[0].crs, geometry='geometry')
+    if 'description' in polygons.columns.values:  # fails to write pama-nyungan polygons to file with this column
+        polygons = polygons.drop('description', axis=1)
+    if save_super:
+        polygons.to_file(f'data/water/tmpAll.{family.name}.gpkg', driver='GPKG')
+
+    super_polygon = polygons.dissolve(as_index=False).to_crs('EPSG:3857')
+    if save_super:
+        super_polygon.to_file(f'data/water/tmpSuper.{family.name}.geojson', driver='GeoJSON')
+
+    print(f'Constructed super polygon in {time.time() - start} seconds!')
+    return super_polygon
+
+
 if __name__ == '__main__':
-    family = PamaNyungan()
-    glottography = PamaNyunganPolygons()
+    family = IndoEuropean()
+    glottography = Glottography(get_config(family.name))
+    # glottography = PamaNyunganPolygons()
 
-    water_file = 'SurfaceHydrologyPolygonsNational.gdb'
-    relevant = read_or_create_relevant_water(family, glottography, water_file)
+    water_file = 'OSM_WaterLayer.pbf'
+    # water_file = 'SurfaceHydrologyPolygonsNational.gdb'
 
+    # super_polygon = get_super_polygon(family, glottography, save_super=True)
+    # start = time.time()
+    # water = geopandas.read_file(f'data/water/{water_file}', layer='multipolygons', mask=super_polygon)  # takes 1042 seconds
+    # print(f'Loaded water in {time.time() - start} seconds')
+    start = time.time()
+    # filter to `water='natural'` etc.
+    water = geopandas.read_file(f'data/water/tmpFiltered.{family.name}.natural.gpkg')
+    print(f'Loaded water in {time.time() - start} seconds')
+
+    # start = time.time()
+    # water = water[water['natural'] == 'water']
+    # print(f'Filtered water in {time.time() - start} seconds')
+    #
+    # start = time.time()
+    # water.to_file(f'data/water/tmpFiltered.{family.name}.natural.gpkg', driver='GPKG')
+    # print(f'Saved water in {time.time() - start} seconds')
+
+    # relevant = read_or_create_relevant_water(family, glottography, water_file)
+    relevant = water.to_crs(LONGLAT)
     dataframesList = []
     df_dict = []
     start = time.time()
     for ascii in tqdm(family.languages_ascii):
         try:
-            p = glottography.get_polygon_from_ascii(family, ascii).to_crs(GEODESIC)
+            p = glottography.get_polygon_from_ascii(family, ascii).to_crs(LONGLAT)
+            start_sub = time.time()
             intersecting = p.overlay(relevant, how='intersection', keep_geom_type=False)
-            merged = intersecting.dissolve(as_index=False).to_crs(GEODESIC)
+            merged = intersecting.dissolve(as_index=False).to_crs(LONGLAT)
             merged = merged.filter(['Name', 'geometry'])
+            print(f'Subset rivers for {ascii} in {time.time() - start_sub} seconds')
 
+            start_sub = time.time()
             points = sample_points(p, N_POINTS)
 
             d = points.apply(lambda point: merged.distance(point))[0]
+            print(f'calculated distances for {ascii} in {time.time() - start_sub} seconds')
             mean_dist = mean(d)
             median_dist = median(d)
             df_dict.append({
