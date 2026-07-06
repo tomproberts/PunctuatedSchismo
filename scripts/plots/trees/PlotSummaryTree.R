ec7b1f7747b3380e39335d68fba6efe2f6e98cac
Thomas Paul Roberts <tomproberts@outlook.com>
2026-07-06 12:00:19 +0200

    Draw time axis on summary tree plot

--- scripts/plots/trees/PlotSummaryTree.R
+++ scripts/plots/trees/PlotSummaryTree.R
@@ -1,16 +1,17 @@
 library(ggplot2)
 library(ggtree)
 library(treeio)
+library(tidytree)
 source("scripts/families/LanguageFamilies.R")
 
-FAMILY <- PAMA.NYUNGAN
-GAMMASPIKE <- FALSE
-SCALESTUBS <- FALSE
+FAMILY <- INDO.EUROPEAN
+GAMMASPIKE <- TRUE
+SCALESTUBS <- TRUE
 
 summary.tree <- paste0("data/", if (GAMMASPIKE) "gammaspike/summarytree/" else "relaxed/", FAMILY, ".nex")
 tree <- read.beast(summary.tree)
 
-tree <- tree_subset(tree, "Garlali", levels_back = 4)
+# tree <- tree_subset(tree, "Garlali", levels_back = 4)
 df <- as_tibble(tree)
 
 aesthetics <- if (GAMMASPIKE) (if (SCALESTUBS)
@@ -18,7 +19,7 @@ aesthetics <- if (GAMMASPIKE) (if (SCALESTUBS)
   aes(color = weightedSpikes_median)) else
   aes(color = log(as.numeric(rate_median)))
 
-ggtree(tree, aesthetics, size = 2,) +
+revts(ggtree(tree, aesthetics, size = 2,) +
   theme_tree2() +  # enable axis
   scale_color_continuous(low = "black", high = if (GAMMASPIKE) "#00bfc4" else "#8494ff") +
   geom_tiplab(color = "black", align = TRUE, as_ylab = TRUE, size = 16) +
@@ -26,4 +27,4 @@ ggtree(tree, aesthetics, size = 2,) +
                                fill = "black", color = "white", alpha = .5)) +
   geom_label2(aes(subset = posterior < 1, label = round(as.numeric(posterior), 2)),
               fill = "white", color = "black", alpha = .8) +
-  theme(legend.position = "none")
+  theme(legend.position = "none"))
