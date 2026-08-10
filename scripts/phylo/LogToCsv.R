f7a65c8486e0cae80cac48239b410ce1fe7397a6
Thomas Paul Roberts <tomproberts@outlook.com>
2026-08-10 14:42:47 +0200

    Fixed bug due to incorrect spikes.i and rates.i indices (spikes start from 0)

--- scripts/phylo/LogToCsv.R
+++ scripts/phylo/LogToCsv.R
@@ -3,29 +3,31 @@ source("scripts/families/LanguageFamilies.R")
 source("scripts/phylo/FullPosterior.R")
 
 set.seed(42)
-GAMMASPIKE <- TRUE
-# LOG.FILE <- "data/phylo/relaxed/full/indoeuropean_1785842680219.log"
-LOG.FILE <- "data/phylo/gammaspike/full/IndoEuropean.log"
-BURN.IN <- 1000
+GAMMASPIKE <- FALSE
+LOG.FILE <- "data/phylo/relaxed/full/indoeuropean_1785842680219.log"
+# LOG.FILE <- "data/phylo/gammaspike/full/IndoEuropean.log"
+BURN.IN <- 1500
+N.SAMPLES <- 200
 
 translation <- get_translation(INDO.EUROPEAN)
-# takes 10-30 seconds
-l <- read.csv(LOG.FILE, sep = "\t", comment.char = "#")
+cat(paste0("Reading ", LOG.FILE, "...\n"))
+l <- read.csv(LOG.FILE, sep = "\t", comment.char = "#")  # takes 10-30 seconds
 all.samples <- l$Sample
 
 # only select branch rate columns
-if (GAMMASPIKE) { rate.param <- "weightedSpikes" } else { rate.param <- "branchRates" }
-rate.cols <- paste(rate.param, unname(translation), sep = ".")
+indices <- unname(translation)
+if (GAMMASPIKE) { rate.param <- "weightedSpikes"; indices <- indices - 1 } else { rate.param <- "branchRates" }
+rate.cols <- paste(rate.param, indices, sep = ".")
 l <- l[, rate.cols]
 
 # select subset of samples
-selected.rows <- sort(BURN.IN + sample(nrow(l) - BURN.IN, 100))
+selected.rows <- sort(BURN.IN + sample(nrow(l) - BURN.IN, N.SAMPLES))
 l <- l[selected.rows,]
 
 # relabel rows, round to 6 digits
 if (GAMMASPIKE) { new.param <- "burst" } else { new.param <- "rate" }
 rownames(l) <- paste(new.param, all.samples[selected.rows], sep = "_")
-l <- format(l, digits = 6)
+l <- format(l, digits = 6, scientific = FALSE)
 
 # transpose, label rows
 df <- data.frame(t(l))
@@ -34,4 +36,6 @@ df <- df[, c(ncol(df), 1:(ncol(df) - 1))]
 
 # write out
 if (GAMMASPIKE) { dir <- "gammaspike" } else { dir <- "relaxed" }
-write.csv(df, paste0("data/phylo/", dir, "/full/IndoEuropean.csv"), quote = FALSE, row.names = FALSE)
+file.out <- paste0("data/phylo/", dir, "/full/IndoEuropean.csv")
+write.csv(df, file.out, quote = FALSE, row.names = FALSE)
+cat(paste("Wrote out", N.SAMPLES, "log samples to", file.out, "\n"))
